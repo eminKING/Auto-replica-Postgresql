5d5d1c495a5dda22bb99210a517efd604f525982
eminKING <79987591+eminKING@users.noreply.github.com>
2025-09-13 12:54:41 +0400

    Update replica-init.sh

--- replica/replica-init.sh
+++ replica/replica-init.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
 
+DATA_DIR="/var/lib/postgresql/data"
+
+# Если data пустая — делаем базовый бэкап
+if [ -z "$(ls -A $DATA_DIR)" ]; then
+  
+  pg_basebackup -h pg_master -D $DATA_DIR -U replica_user -P -R
+  
+fi
