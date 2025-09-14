b9ceadf85c8ec163ff42a5dda743d7d14342a9f1
eminKING <79987591+eminKING@users.noreply.github.com>
2025-09-14 16:39:48 +0400

    Update entrypoint.sh

--- replica/entrypoint.sh
+++ replica/entrypoint.sh
@@ -8,6 +8,12 @@ MASTER_PASSWORD="replica_pass"
 
 export PGPASSWORD="$MASTER_PASSWORD"
 
+# Ждём, пока мастер станет доступен
+until pg_isready -h $MASTER_HOST -p 5432 -U $MASTER_USER; do
+    echo "Waiting for master to start..."
+    sleep 2
+done
+
 # Если data пустая — делаем базовый бэкап
 if [ -z "$(ls -A $DATA_DIR)" ]; then
   pg_basebackup -h $MASTER_HOST -D $DATA_DIR -U $MASTER_USER -P -R --wal-method=stream
