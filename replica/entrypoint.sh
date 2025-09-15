dabfa9517d6fb724e534261e8ce5055017a424eb
eminKING <79987591+eminKING@users.noreply.github.com>
2025-09-15 10:35:07 +0400

    Update entrypoint.sh

--- replica/entrypoint.sh
+++ replica/entrypoint.sh
@@ -6,6 +6,13 @@ MASTER_HOST="pg_master"
 MASTER_USER="replica_user"
 MASTER_PASSWORD="replica_pass"
 
+# Исправляем права на папку данных
+if [ "$(stat -c %u $DATA_DIR)" != "999" ]; then
+    echo "Fixing permissions on $DATA_DIR..."
+    chown -R 999:999 $DATA_DIR
+    chmod 700 $DATA_DIR
+fi
+
 export PGPASSWORD="$MASTER_PASSWORD"
 
 # Ждём, пока мастер станет доступен
