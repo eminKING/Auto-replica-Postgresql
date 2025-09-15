86fd7d4ed588bd8a3e9e7185c48dd83a270dbf02
eminKING <79987591+eminKING@users.noreply.github.com>
2025-09-15 17:51:24 +0400

    Update init-replica-user.sql

--- master/init-replica-user.sql
+++ master/init-replica-user.sql
@@ -1,12 +1,1 @@
-CREATE DATABASE test_db;
-
-\c test_db
-
-CREATE TABLE test_replication (
-    id SERIAL PRIMARY KEY,
-    message TEXT
-);
-
-INSERT INTO test_replication(message) VALUES ('Hello from master!');
-
 CREATE ROLE replica_user WITH REPLICATION LOGIN ENCRYPTED PASSWORD 'replica_pass';
