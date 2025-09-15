4425d9dacd2dfc7ae85ad28e2907bd6193b8d403
eminKING <79987591+eminKING@users.noreply.github.com>
2025-09-15 11:47:54 +0400

    Create start_postgres.sh

---
+++ start_postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+mkdir -p ./master/master-data ./replica/replica-data
+
+# Выставляем права для Postgres (uid:gid 999:999)
+sudo chown -R 999:999 ./master/master-data ./replica/replica-data
+sudo chmod 700 ./master/master-data ./replica/replica-data
+
+docker-compose up -d
